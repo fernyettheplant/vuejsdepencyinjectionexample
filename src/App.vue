f4963168589b00ab971623c010f9f56d2177f424
Jesús Fernando Alvarez Franco <jefer_590@hotmail.com>
2019-05-25 12:58:57 -0500

    :lipstick: - Add Template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1>Kanye West Quotes</h1>
+    <p>Quote: {{ quote }}</p>
+    <button @click="getQuote">Get a quote!</button>
   </div>
 </template>
 
@@ -7,5 +10,13 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  private quote: string = ''
+
+  private getQuote (): Promise<void> {
+    console.log('Click!')
+
+    return Promise.resolve()
+  }
+}
 </script>
